4b85b25edca46c6aadec2572c33ae2821c0bc056
VINODvoid <iamlearner.v@gmail.com>
2025-07-26 23:09:45 +0530

    feat: integrate Inngest agent for code generation and update input handling
    - Added @inngest/agent-kit dependency for agent functionality.
    - Created a new Input component for user input.
    - Updated Home component to include input field and handle value changes.
    - Modified TRPC router to accept 'value' instead of 'text' for invoking jobs.
    - Enhanced Inngest function to utilize the agent for generating code snippets based on user input.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import {toast}  from "sonner";
 
 /**
@@ -11,6 +13,8 @@ import {toast}  from "sonner";
  * The button is disabled while the mutation is pending to prevent multiple invocations.
  */
 export default function Home() {
+  const [value,setValue] = useState<string>("");
+
  const trpc =useTRPC();
  const invoke = useMutation(trpc.invoke.mutationOptions({
   onSuccess:()=>{
@@ -20,7 +24,8 @@ export default function Home() {
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <Button onClick={()=> invoke.mutate({text:"hello"})} disabled={invoke.isPending}>
+      <Input onChange={(e)=> setValue(e.target.value)} value={value}/>
+      <Button onClick={()=> invoke.mutate({value:value})} disabled={invoke.isPending}>
       Invoke Job
       </Button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,19 @@
 import { inngest } from "./client";
+import {  gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    const codeAgent  = createAgent({
+      name:"code-agent",
+      system:"You are an export nextjs developer. You write readable, maintainable code, you write simple next.js & react snippets ",
+      model:gemini({model:"gemini-2.0-flash"})
+    });
+   
+    const {output } = await codeAgent.run(
+      `Write the following snippets for : ${event.data.value}`
+    )
+    return { output};
+  }
 );
