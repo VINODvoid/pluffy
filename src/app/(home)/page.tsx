4d287e9e696929cad6211f964dc457bb6940949b
VINODvoid <kalki.the.dev@gmail.com>
2025-09-26 13:03:44 +0530

    chore: clean up whitespace in home page component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -23,6 +23,8 @@ export default function Home() {
   //   })
   // );
 
+
+  
   return (
     <div className="flex flex-col max-w-5xl mx-auto w-full">
       <section className="space-y-6 py-[16vh] 2xl:py-48">
